5f05844d52843407d07b23a30d52a2f10658b005
Vinalynn <william.cai@live.com>
2013-07-29 21:48:21 +0800

    again

---
+++ src/main/java/org/vinalynn/wapp/wmblog/data/DataBean.java
@@ -0,0 +1,27 @@
+package org.vinalynn.wapp.wmblog.data;
+
+/**
+ * User: caiwm
+ * Date: 13-7-29
+ * Time: ÏÂÎç5:09
+ */
+public abstract class DataBean {
+    private String kind;
+    private String uuid;
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}
---
+++ src/main/java/org/vinalynn/wapp/wmblog/web/controller/GuestbookController.java
@@ -0,0 +1,49 @@
+package org.vinalynn.wapp.wmblog.web.controller;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.vinalynn.wapp.wmblog.GlobalConst;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * User: caiwm
+ * Date: 13-7-26
+ * Time: ÏÂÎç1:45
+ */
+@Controller
+public class GuestbookController {
+
+    /**
+     *
+     * @param request
+     * @param model
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value="/u-info.request")
+    public String t_ServiceUser(HttpServletRequest request, Model model) throws Exception{
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+        String loginUrl = userService.createLoginURL(request.getRequestURI());
+        if(null == user){
+            model.addAttribute(GlobalConst.FTL_COMMON_MSG_KEY,
+                    "<a href='"+loginUrl+"'> Sign In </a>"
+            );
+        }else{
+            model.addAttribute(GlobalConst.FTL_COMMON_MSG_KEY,
+                    user.getNickname()
+            );
+        }
+
+        return GlobalConst.FTL_COMMON_MSG_URL;
+    }
+
+
+}
