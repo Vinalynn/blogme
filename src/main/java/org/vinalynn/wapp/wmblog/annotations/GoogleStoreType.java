c35c3fba69f128214d420af7aa98296de12ec100
Vinalynn <william.cai@live.com>
2013-07-30 17:04:35 +0800

    因为Google DataStore的Entity对于属性的大小要求比较严格，所以对于比较大的String采用DataStore的Text来存取，所以在存取的时候要多封装一层。这次采用注解的方式来生命需要转换成Text的字段。

--- src/main/java/org/vinalynn/wapp/wmblog/annotations/GoogleStoreType.java
+++ src/main/java/org/vinalynn/wapp/wmblog/annotations/GoogleStoreType.java
@@ -8,6 +8,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * <p>因为Google DataStore的Entity对于属性的大小要求比较严格，
+ * 所以对于比较大的String采用DataStore的Text来存取，所以在存取
+ * 的时候要多封装一层。这次采用注解的方式来生命需要转换成Text的字
+ * 段。</p>
  * User: caiwm
  * Date: 13-7-30
  * Time: 上午10:29
